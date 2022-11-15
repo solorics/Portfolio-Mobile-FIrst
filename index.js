4d61021da48e39f2c0f7fccb8754cf5f1f72cca4
Leehaney gee <georgeleehaney20@gmail.com>
2022-11-15 17:57:53 +0300

    Feat: Add variables to javascript

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const hamburgerMenu = document.querySelector('.hamburger');
+const closeButton = document.querySelector('.closeBtn');
+const popNav = document.querySelectorAll('.pop-nav');
+
 function openNav() {
   document.getElementById('mobileNav').style.display = 'block';
 }
@@ -6,9 +10,9 @@ function closeNav() {
   document.getElementById('mobileNav').style.display = 'none';
 }
 
-document.querySelector('.hamburger').addEventListener('click', openNav);
-document.querySelector('.closeBtn').addEventListener('click', closeNav);
-document.querySelectorAll('.pop-nav').forEach((link) => {
+hamburgerMenu.addEventListener('click', openNav);
+closeButton.addEventListener('click', closeNav);
+popNav.forEach((link) => {
   link.addEventListener('click', () => {
     document.getElementById('mobileNav').style.display = 'none';
   });
