46a211e66818642e13559886e14b7602ca76ee64
solomon opeyemi <opeyemisolomon906@gmail.com>
2022-11-19 01:31:33 +0100

    pop up still at work

--- index.js
+++ index.js
@@ -17,168 +17,269 @@ popNav.forEach((link) => {
     document.getElementById('mobileNav').style.display = 'none';
   });
 });
+
+// This is the section for DOM manipulation using javaScript.
+
 // Card creation for mobile version.
-// const projects = [{
-//   id: '1',
-//   title: 'Project name goes here',
-//   features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
-//   image: './assets/images/Rectangle21.svg',
-//   img: './assets/images/Union.svg',
-//   sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
-//   sideImage: [
-//       './assets/images/Rectangle21.svg',
-//       './assets/images/Rectangle21.svg',
-//       './assets/images/Rectangle21.svg',
-//       './assets/images/Rectangle21.svg',
-//   ],
-//   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-// eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-// enim ad minim veniam, quis nostrud exercitation ullamco laboris
-// <br /><br />
-// nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-// amet, consectetur adipiscing elit, sed do eiusmod tempor
-// incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-// veniam, quis nostrud exercitation ullamco laboris nisi.`,
-//   linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
-//   linkToLive: 'https://github.com/NurkaAmre',
-//   btn: 'See Source',
-//   side: ['Next Project', 'Previous Project'],
-//   icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
-//   btnForPopup: ['See Live','See Source']
-// }, {
-//   id: '2',
-//   title: 'Project name goes here',
-//   features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
-//   image: './assets/images/Rectangle22.svg',
-//   img: './assets/images/Union.svg',
-//   sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
-//   sideImage: [
-//       './assets/images/Rectangle22.svg',
-//       './assets/images/Rectangle22.svg',
-//       './assets/images/Rectangle22.svg',
-//       './assets/images/Rectangle22.svg',
-//   ],
-//   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-// eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-// enim ad minim veniam, quis nostrud exercitation ullamco laboris
-// <br /><br />
-// nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-// amet, consectetur adipiscing elit, sed do eiusmod tempor
-// incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-// veniam, quis nostrud exercitation ullamco laboris nisi.`,
-//   linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
-//   linkToLive: 'https://github.com/NurkaAmre',
-//   btn: 'See Source',
-//   side: ['Next Project', 'Previous Project'],
-//   icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
-//   btnForPopup: ['See Live','See Source']
-// }, {
-//   id: '3',
-//   title: 'Project name goes here',
-//   features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
-//   image: './assets/images/Rectangle23.svg',
-//   img: './assets/images/Union.svg',
-//   sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
-//   sideImage: [
-//       './assets/images/Rectangle23.svg',
-//       './assets/images/Rectangle23.svg',
-//       './assets/images/Rectangle23.svg',
-//       './assets/images/Rectangle23.svg',
-//   ],
-//   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-// eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-// enim ad minim veniam, quis nostrud exercitation ullamco laboris
-// <br /><br />
-// nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-// amet, consectetur adipiscing elit, sed do eiusmod tempor
-// incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-// veniam, quis nostrud exercitation ullamco laboris nisi.`,
-//   linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
-//   linkToLive: 'https://github.com/NurkaAmre',
-//   btn: 'See Source',
-//   side: ['Next Project', 'Previous Project'],
-//   icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
-//   btnForPopup: ['See Live','See Source']
-// }, {
-//   id: '4',
-//   title: 'Project name goes here',
-//   features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
-//   image: './assets/images/Rectangle24.svg',
-//   img: './assets/images/Union.svg',
-//   sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
-//   sideImage: [
-//       './assets/images/Rectangle24.svg',
-//       './assets/images/Rectangle24.svg',
-//       './assets/images/Rectangle24.svg',
-//       './assets/images/Rectangle24.svg',
-//   ],
-//   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-// eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-// enim ad minim veniam, quis nostrud exercitation ullamco laboris
-// <br /><br />
-// nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-// amet, consectetur adipiscing elit, sed do eiusmod tempor
-// incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-// veniam, quis nostrud exercitation ullamco laboris nisi.`,
-//   linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
-//   linkToLive: 'https://github.com/NurkaAmre',
-//   btn: 'See Source',
-//   side: ['Next Project', 'Previous Project'],
-//   icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
-//   btnForPopup: ['See Live','See Source']
-// }, {
-//   id: '5',
-//   title: 'Project name goes here',
-//   features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
-//   image: './assets/images/Rectangle25.svg',
-//   img: './assets/images/Union.svg',
-//   sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
-//   sideImage: [
-//       './assets/images/Rectangle25.svg',
-//       './assets/images/Rectangle25.svg',
-//       './assets/images/Rectangle25.svg',
-//       './assets/images/Rectangle25.svg',
-//   ],
-//   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-// eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-// enim ad minim veniam, quis nostrud exercitation ullamco laboris
-// <br /><br />
-// nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-// amet, consectetur adipiscing elit, sed do eiusmod tempor
-// incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-// veniam, quis nostrud exercitation ullamco laboris nisi.`,
-//   linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
-//   linkToLive: 'https://github.com/NurkaAmre',
-//   btn: 'See Source',
-//   side: ['Next Project', 'Previous Project'],
-//   icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
-//   btnForPopup: ['See Live','See Source']
-// },  {
-//   id: '6',
-//   title: 'Project name goes here',
-//   features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
-//   image: './assets/images/Rectangle26.svg',
-//   img: './assets/images/Union.svg',
-//   sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
-//   sideImage: [
-//       './assets/images/Rectangle26.svg',
-//       './assets/images/Rectangle26.svg',
-//       './assets/images/Rectangle26.svg',
-//       './assets/images/Rectangle26.svg',
-//   ],
-//   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-// eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-// enim ad minim veniam, quis nostrud exercitation ullamco laboris
-// <br /><br />
-// nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-// amet, consectetur adipiscing elit, sed do eiusmod tempor
-// incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-// veniam, quis nostrud exercitation ullamco laboris nisi.`,
-//   linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
-//   linkToLive: 'https://github.com/NurkaAmre',
-//   btn: 'See Source',
-//   side: ['Next Project', 'Previous Project'],
-//   icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
-//   btnForPopup: ['See Live','See Source']
-// },
-// ];
+const projects = [{
+  id: '1',
+  title: 'Project name goes here',
+  features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  image: './assets/images/Rectangle21.svg',
+  img: './assets/images/Union.svg',
+  sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
+  sideImage: [
+    './assets/images/Rectangle21.svg',
+    './assets/images/Rectangle21.svg',
+    './assets/images/Rectangle21.svg',
+    './assets/images/Rectangle21.svg',
+  ],
+  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+enim ad minim veniam, quis nostrud exercitation ullamco laboris
+<br /><br />
+nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi.`,
+  linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
+  linkToLive: 'https://github.com/NurkaAmre',
+  btn: 'See Source',
+  side: ['Next Project', 'Previous Project'],
+  icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
+  btnForPopup: ['See Live', 'See Source'],
+}, {
+  id: '2',
+  title: 'Project name goes here',
+  features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  image: './assets/images/Rectangle22.svg',
+  img: './assets/images/Union.svg',
+  sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
+  sideImage: [
+    './assets/images/Rectangle22.svg',
+    './assets/images/Rectangle22.svg',
+    './assets/images/Rectangle22.svg',
+    './assets/images/Rectangle22.svg',
+  ],
+  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+enim ad minim veniam, quis nostrud exercitation ullamco laboris
+<br /><br />
+nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi.`,
+  linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
+  linkToLive: 'https://github.com/NurkaAmre',
+  btn: 'See Source',
+  side: ['Next Project', 'Previous Project'],
+  icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
+  btnForPopup: ['See Live', 'See Source'],
+}, {
+  id: '3',
+  title: 'Project name goes here',
+  features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  image: './assets/images/Rectangle23.svg',
+  img: './assets/images/Union.svg',
+  sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
+  sideImage: [
+    './assets/images/Rectangle23.svg',
+    './assets/images/Rectangle23.svg',
+    './assets/images/Rectangle23.svg',
+    './assets/images/Rectangle23.svg',
+  ],
+  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+enim ad minim veniam, quis nostrud exercitation ullamco laboris
+<br /><br />
+nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi.`,
+  linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
+  linkToLive: 'https://github.com/NurkaAmre',
+  btn: 'See Source',
+  side: ['Next Project', 'Previous Project'],
+  icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
+  btnForPopup: ['See Live', 'See Source'],
+}, {
+  id: '4',
+  title: 'Project name goes here',
+  features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  image: './assets/images/Rectangle24.svg',
+  img: './assets/images/Union.svg',
+  sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
+  sideImage: [
+    './assets/images/Rectangle24.svg',
+    './assets/images/Rectangle24.svg',
+    './assets/images/Rectangle24.svg',
+    './assets/images/Rectangle24.svg',
+  ],
+  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+enim ad minim veniam, quis nostrud exercitation ullamco laboris
+<br /><br />
+nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi.`,
+  linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
+  linkToLive: 'https://github.com/NurkaAmre',
+  btn: 'See Source',
+  side: ['Next Project', 'Previous Project'],
+  icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
+  btnForPopup: ['See Live', 'See Source'],
+}, {
+  id: '5',
+  title: 'Project name goes here',
+  features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  image: './assets/images/Rectangle25.svg',
+  img: './assets/images/Union.svg',
+  sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
+  sideImage: [
+    './assets/images/Rectangle25.svg',
+    './assets/images/Rectangle25.svg',
+    './assets/images/Rectangle25.svg',
+    './assets/images/Rectangle25.svg',
+  ],
+  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+enim ad minim veniam, quis nostrud exercitation ullamco laboris
+<br /><br />
+nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi.`,
+  linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
+  linkToLive: 'https://github.com/NurkaAmre',
+  btn: 'See Source',
+  side: ['Next Project', 'Previous Project'],
+  icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
+  btnForPopup: ['See Live', 'See Source'],
+}, {
+  id: '6',
+  title: 'Project name goes here',
+  features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  image: './assets/images/Rectangle26.svg',
+  img: './assets/images/Union.svg',
+  sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
+  sideImage: [
+    './assets/images/Rectangle26.svg',
+    './assets/images/Rectangle26.svg',
+    './assets/images/Rectangle26.svg',
+    './assets/images/Rectangle26.svg',
+  ],
+  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+enim ad minim veniam, quis nostrud exercitation ullamco laboris
+<br /><br />
+nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi.`,
+  linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
+  linkToLive: 'https://github.com/NurkaAmre',
+  btn: 'See Source',
+  side: ['Next Project', 'Previous Project'],
+  icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
+  btnForPopup: ['See Live', 'See Source'],
+},
+];
+
+const body = document.querySelector('.workSection');
+
+for (i = 0; i < projects.length; i += 1) {
+  const projectSection = document.createElement('section');
+  body.appendChild(projectSection);
+  projectSection.innerHTML = ` 
+    <div class="container container-information bottom modal-b">
+    <li class="span-3" project-item desktop-grid">
+            <img
+              class="project-img"
+              src="${projects[i].image}"
+              alt="laptop"
+            />
+            <div class="container container-information bottom">
+              <h2 class="heading heading-desktop">${projects[i].title}</h2> 
+              <ul class="features">
+                <li class="feature feature-desktop">${projects[i].features[0]}</li>
+                <li class="feature feature-desktop">${projects[i].features[1]}</li>
+                <li class="feature feature-desktop">${projects[i].features[2]}</li>
+              </ul>
+              <button class="c-projects see-source" id=${projects[i].id} onclick="popUp(this.id)">
+                ${projects[i].btn}
+                <img src="${projects[i].img}" alt="arrow" />
+              </button>
+            </div>
+        </li>
+    </div>
+    `;
+}
+
+function popUp(item) {
+  console.log(item);
+  const overlay = document.querySelector('.extra');
+
+  overlay.style.display = 'flex';
+  for (let j = 0; j < projects.length; j += 1) {
+    if (item.toString() === projects[j].id.toString()) {
+      const popupModal = document.querySelector('.popup-modal');
+      console.log(popupModal);
+      popupModal.innerHTML = `
+    <div class="container bottom ">
+    <div class="for-x">
+    <h2 class="modal-heading">${projects[item].title}</h2>
+    <button type="button" class="menu-btn">
+              &#x2716
+            </button>
+    </div>
+    <ul class="modal-features">
+      <li class="feature-modal">${projects[item].features[0]}</li>
+      <li class="feature-modal">${projects[item].features[1]}</li>
+      <li class="feature-modal">${projects[item].features[2]}</li>
+    </ul>
+    <div class="next-pre">
+    <img class="modal-img" src="${projects[item].image}" alt="laptop-img" />
+    <img class="next" src="${projects[item].sideArrows[0]}" alt="previous"} />
+    <img class="pre" src="${projects[item].sideArrows[1]}" alt="next"} />
+    </div>
+    <ul class="list-img" role="list">
+              <li role="listItem" class="choose">
+                <img class="img1" src="${projects[item].sideImage[0]}" alt="project" />
+              </li>
+              <li role="listItem">
+                <img class="img1" src="${projects[item].sideImage[1]}" alt="project" />
+              </li>
+              <li role="listItem">
+                <img class="img1" src="${projects[item].sideImage[2]}" alt="project" />
+              </li>
+              <li role="listItem">
+                <img class="img1" src="${projects[item].sideImage[3]}" alt="project" />
+              </li>
+            </ul>
+    <div>
+       <div class="modal-text"><p>${projects[item].description}</p></div>
+    <div class="modal-links">
+       <button class="see-more">
+         ${projects[item].btnForPopup[0]}
+        <img class="arrow" src="${projects[item].icons[1]}" alt="arrow" />
+       </button>
+       <button class="see-more">
+         ${projects[item].btnForPopup[1]}
+        <img class="arrow" src="${projects[item].icons[0]}" alt="arrow" />
+        </button>
+    </div>
+    <div>
+        <button>
+         <span>${projects[item].side[0]}</span>
+        </button>
+              <button>
+                <span>${projects[item].side[1]}</span>
+              </button>
+        </div>
+        </div>
+    </div>
+    `;
+    }
+  }
+}
